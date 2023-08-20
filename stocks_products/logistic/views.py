14673485e8b68412c7a2517798b7ecd7959530b0
Sergey P <ponomarevsergej@gmail.com>
2023-08-20 23:57:44 +0300

    add -text1

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -29,4 +29,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def test_page(request):
-    return Response('Оно работает!')
+    return Response('Нефига не ясно! но очень интересно!!!!')
